1a27fbab3a736d1ed942f144b693225aaeff10d1
Laxmi Nyamagoud <laxmi.nyamagoud@intimetec.com>
2026-02-07 09:39:50 +0530

    Added routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router'
+import Main from './components/Main.jsx'
+import Login from './components/Login.jsx'
+import Profile from './components/Profile.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h2>Hello World!</h2>
+      <BrowserRouter basename='/'>
+        <Routes>
+          <Route path='/' element={<Main />}>
+            <Route path="/login" element={<Login />} />
+            <Route path="/profile" element={<Profile />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
